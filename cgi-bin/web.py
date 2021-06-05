65d89ec1527d803dc9c1501a10e1133ef9714784
Pitan MAD <58260965+PTOM76@users.noreply.github.com>
2021-06-05 18:53:29 +0900

    Add files via upload

--- cgi-bin/web.py
+++ cgi-bin/web.py
@@ -1,11 +1,17 @@
 import random
+import sys
+import io
 
-print("Content-type: text/html;charset=utf-8\n");
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
 r = random.randint(1,6);
 string = "サイコロを振り、<b>" + str(r) + "</b>がでました。<br /><img style=\"width:50px;height:50px;border: 1px solid;\" src=\"../img/dice_" + str(r) + ".png\" />";
+print("Content-type: text/html;charset=utf-8\n");
 print("""
 <html>
-    <head></head>
+    <head>
+    	<meta http-equiv="content-type" charset="UTF-8">
+	</head>
     <body>
         """+ string +"""
     </body>
